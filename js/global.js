ec4ef4c1fbec476f968f4aba22650386d76081fd
Sophia Khan <sophiaxkhan@gmail.com>
2017-02-21 13:31:48 -0500

    Updated movie trailers

--- js/global.js
+++ js/global.js
@@ -1,12 +1,14 @@
 // 1. Make a class called Trailer that extends Movie
-class _____ extends _____ {
+class Trailer extends Movie {
 
     // 2. Inside the class, add a constructor function that takes two arguments, a name and an id
-    constructor() {
+    constructor(movieTitle, movieID) {
 
         // 3. Call the parent constructor function using the super function
-
+        super(movieTitle, movieID);
         // 4. Assign a name and id instance property to the incoming name and id arguments
+        this.name = movieTitle;
+        this.id = movieID;
 
         // 5. This method will show the button on the screen, no changes necessary
         this.renderButton()
@@ -14,3 +16,11 @@ class _____ extends _____ {
 }
 
 // 6. Create four instances of the Trailer class, passing in the name of a film as the first argument and its corresponding YouTube ID as the second argument
+
+var theAddamsFamily = new Trailer('The Addams Family', 'LyyJYyIexq8');
+
+var halloweenFilm = new Trailer('Halloween', 'DLEk6hES03Q');
+
+var fridayThe13th = new Trailer('Friday the 13th', 'WCO4v-pFBns');
+
+var psychoFilm = new Trailer('Psycho', 'Wz719b9QUqY');
